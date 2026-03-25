5b046f25a2916ac04f0febe025010161b81ff240
Erick <erickmelvzlpz@gmail.com>
2026-03-24 22:05:22 -0600

    [CHORE] Creating dockerfiles and adding them to the dockercompose

--- services/tracking-service/src/main/java/com/healthcore/tracking/service/CatalogGrpcClientService.java
+++ services/tracking-service/src/main/java/com/healthcore/tracking/service/CatalogGrpcClientService.java
@@ -5,6 +5,7 @@ import com.healthcore.catalog.grpc.FoodResponse;
 import com.healthcore.catalog.grpc.NutritionalCatalogGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,23 +13,20 @@ public class CatalogGrpcClientService {
 
     private final NutritionalCatalogGrpc.NutritionalCatalogBlockingStub catalogStub;
 
-    public CatalogGrpcClientService() {
-        // 1. Abrimos un tubo directo y ultrarrápido hacia el microservicio de Python
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
-                .usePlaintext() // Sin encriptación por ahora, es tráfico interno
+    public CatalogGrpcClientService(@Value("${grpc.catalog.target:localhost:50051}") String grpcTarget) {
+
+        ManagedChannel channel = ManagedChannelBuilder.forTarget(grpcTarget)
+                .usePlaintext()
                 .build();
 
-        // 2. Creamos el "Stub" (nuestro cliente autogenerado)
         this.catalogStub = NutritionalCatalogGrpc.newBlockingStub(channel);
     }
 
     public FoodResponse getFoodMacros(String barcode) {
-        // 3. Empacamos el código de barras en el formato binario de gRPC
         FoodRequest request = FoodRequest.newBuilder()
                 .setBarcode(barcode)
                 .build();
 
-        // 4. ¡Disparamos la petición a Python y recibimos la respuesta al instante!
         return catalogStub.getFoodItem(request);
     }
 }

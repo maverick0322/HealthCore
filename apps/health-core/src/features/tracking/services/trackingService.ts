6f5ae4da5c70eec1bddb8f125ce752e276e1b137
Erick <erickmelvzlpz@gmail.com>
2026-05-17 12:21:25 -0600

    [FIX] Cleaned smell comments fron tracking front

--- apps/health-core/src/features/tracking/services/trackingService.ts
+++ apps/health-core/src/features/tracking/services/trackingService.ts
@@ -3,7 +3,6 @@ import type { LogFoodRequest, LogFoodResponse } from '../types/tracking.types';
 
 export const trackingService = {
   logFood: async (data: LogFoodRequest): Promise<LogFoodResponse> => {
-    // ¡Aquí estaba el detalle! Cambiamos /food por /meal
     const response = await httpClient.post<LogFoodResponse>('/tracking/logs/meal', data);
     return response.data;
   },
